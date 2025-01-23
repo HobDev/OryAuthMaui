196c099f1d9df74725770a2ef4298b2878f79296
Paramjit <sparamjit92@gmail.com>
2025-01-23 11:16:35 +0530

    delete

--- OryAuthMaui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace OryAuthMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
---
+++ OryAuthNetMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OryAuthNetMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
