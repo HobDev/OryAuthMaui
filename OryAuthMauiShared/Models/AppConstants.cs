c1b9bbf503240c27761788b017a472ce38f95239
Paramjit <sparamjit92@gmail.com>
2025-02-10 20:44:47 +0530

    add to gitignore

--- OryAuthMauiShared/Models/AppConstants.cs
+++ OryAuthMauiShared/Models/AppConstants.cs
@@ -4,5 +4,5 @@ namespace OryAuthMauiShared.Models;
 
 public static class AppConstants
 {
-   public const string BaseUrl="https://kind-kapitsa-rmqphy91zf.projects.oryapis.com";
+   public  const string BaseUrl="https://kind-kapitsa-rmqphy91zf.projects.oryapis.com";
 }
